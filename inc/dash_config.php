d7fc8ba359da46bbc52a5de219b77a808312b022
Antonin <antonin.georges@gmail.com>
2017-09-01 10:20:50 +0200

    V1.0.4

--- inc/dash_config.php
+++ inc/dash_config.php
@@ -1,6 +1,6 @@
 <?php
 $servername = "Usine Logiciel"; // server title
-$server_port = "";
+$server_port = "443";
 $home_www_addr = "https://openshift.eul.sncf.fr"; //your domain name without  "/"
 $server_address = "https://openshift.eul.sncf.fr"; // server address
 $srv_requests_addr = "https://openshift.eul.sncf.fr"; //  requests adress , youj may use ports as well
