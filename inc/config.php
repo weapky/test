9b5dda301e0f47a5cf56894dc2ea7a39351495ec
Antonin <antonin.georges@gmail.com>
2017-09-01 09:59:03 +0200

    remove old conf

--- inc/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$apikey = "8d71c94b3ba36b3b30eca0711a0feeea"; //your plexpy api key
-$plexpy_url = "192.168.0.81:8080"; //plexpy url without "/"
-$section_id_movies = "13"; //plex section id for movies. may be 1,2,3,4,5,6 etc
-$section_id_shows = "22";  //plex section id for tv shows may be 1.2.3.4.5.6 tec
-$tvdb_api = "427401599ECBDCBE"; //tvdb api
-
-?>
--- inc/dash_config.php
+++ inc/dash_config.php
@@ -1,11 +1,13 @@
 <?php
 $servername = "Usine Logiciel"; // server title
+$server_port = "80"; 2400
 $home_www_addr = "https://openshift.eul.sncf.fr"; //your domain name without  "/"
 $server_address = "https://openshift.eul.sncf.fr"; // server address
 $srv_requests_addr = "https://openshift.eul.sncf.fr"; //  requests adress , youj may use ports as well
 $server_ip = "https://openshift.eul.sncf.fr"
+
 //server status 
 
-$server_port = "80"; 2400
+
 //$copyright = "Service Team e.SNCF"
 ?>
