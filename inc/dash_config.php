1eec37fc804e4fbdbca77d867caac7796ba90240
Antonin <antonin.georges@gmail.com>
2017-09-01 11:13:40 +0200

    V1.1.7

--- inc/dash_config.php
+++ inc/dash_config.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "Usine Logiciel"; // server title
 $server_port = "80";
-$home_www_addr = "https://openshift.eul.sncf.fr"; //your domain name without  "/"
-$server_address = "https://openshift.eul.sncf.fr"; // server address
+$home_www_addr = "http://weapky.fr"; //your domain name without  "/"
+$server_address = "http://weapky.fr"; // server address
 $srv_requests_addr = "https://openshift.eul.sncf.fr"; //  requests adress , youj may use ports as well
 $server_ip = "weapky.fr"
 
