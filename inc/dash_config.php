21d0d57411a29b9f25b24d6441d05158960a17ca
Antonin <antonin.georges@gmail.com>
2017-09-01 10:00:08 +0200

    v7

--- inc/dash_config.php
+++ inc/dash_config.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "Usine Logiciel"; // server title
 $server_port = "80"; 2400
-$home_www_addr = "https://openshift.eul.sncf.fr"; //your domain name without  "/"
+//$home_www_addr = "https://openshift.eul.sncf.fr"; //your domain name without  "/"
 $server_address = "https://openshift.eul.sncf.fr"; // server address
 $srv_requests_addr = "https://openshift.eul.sncf.fr"; //  requests adress , youj may use ports as well
 $server_ip = "https://openshift.eul.sncf.fr"
